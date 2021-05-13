31b69b3c4e3ecc7e793a7d7916da4c02efec6ffb
Hugo Gresse <hugo.gresse@gmail.com>
2021-05-13 19:05:40 +0200

    Add CRA, change folder structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hi Bob
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
