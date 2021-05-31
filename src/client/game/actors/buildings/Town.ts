60abfef87da78988a208e3b06ff68a35d671c6dc
Hugo Gresse <hugo.gresse@gmail.com>
2021-05-31 15:29:00 +0200

    Fix it tile selection

--- src/client/game/actors/buildings/Town.ts
+++ src/client/game/actors/buildings/Town.ts
@@ -10,7 +10,6 @@ export class Town extends Building {
         super(scene, x, y, BUILDING_TOWN)
 
         this.playerRectangle = this.scene.add.rectangle(this.x +TILE_WIDTH_HEIGHT/2, this.y+TILE_WIDTH_HEIGHT/2 , TILE_WIDTH_HEIGHT, TILE_WIDTH_HEIGHT);
-        console.log(player.color, Display.Color.HexStringToColor(player.color))
         this.playerRectangle.setStrokeStyle(3, Display.Color.HexStringToColor(player.color).color)
     }
 }
