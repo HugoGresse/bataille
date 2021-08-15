02d0cfcb2f3084b64be9b03e1759ec281f261b63
Hugo Gresse <hugo.gresse@gmail.com>
2021-08-15 20:29:51 +0200

    test prettier?

--- src/client/game/scenes/UI/BuildingOverlay.ts
+++ src/client/game/scenes/UI/BuildingOverlay.ts
@@ -1,8 +1,8 @@
-import {UIScene} from './UIScene'
-import {Town} from '../../actors/buildings/Town'
-import {getGameWindowSize} from '../../../utils/getGameWindowSize'
-import {UnitsType} from '../../../../common/UNITS'
-import {TEXT_STYLE} from '../../../utils/TEXT_STYLE'
+import { UIScene } from './UIScene'
+import { Town } from '../../actors/buildings/Town'
+import { getGameWindowSize } from '../../../utils/getGameWindowSize'
+import { UnitsType } from '../../../../common/UNITS'
+import { TEXT_STYLE } from '../../../utils/TEXT_STYLE'
 
 const OVERLAY_WIDTH = 200
 const OVERLAY_HEIGHT = 50
@@ -10,70 +10,123 @@ const OVERLAY_PADDING = 10
 
 type Shapes = Phaser.GameObjects.Rectangle | Phaser.GameObjects.Text
 
-const NEW_UNIT_SHORTCUT_BIS = "R"
-const NEW_UNIT_10X_SHORTCUT_BIS = "T"
+const NEW_UNIT_SHORTCUT_BIS = 'R'
+const NEW_UNIT_10X_SHORTCUT_BIS = 'T'
 
 export class BuildingOverlay {
-
     shapes: Shapes[] = []
     selectedTown: Town | null = null
 
-    constructor(private scene: UIScene) {
-    }
+    constructor(private scene: UIScene) {}
 
     onTownSelected(town: Town) {
-        if(this.selectedTown ){
+        if (this.selectedTown) {
             this.onEmptyTileSelected()
         }
-        const {width, height} = getGameWindowSize(this.scene)
-        const rectangle = this.scene.add.rectangle(width/2, height - OVERLAY_HEIGHT/2, OVERLAY_WIDTH, OVERLAY_HEIGHT)
-        rectangle.setFillStyle(0xFF0000, 1)
+        const { width, height } = getGameWindowSize(this.scene)
+        const rectangle = this.scene.add.rectangle(
+            width / 2,
+            height - OVERLAY_HEIGHT / 2,
+            OVERLAY_WIDTH,
+            OVERLAY_HEIGHT
+        )
+        rectangle.setFillStyle(0xff0000, 1)
 
-        const newUnitText = this.scene.add.text(width/2 - OVERLAY_WIDTH/2 + OVERLAY_PADDING, height - OVERLAY_HEIGHT + OVERLAY_PADDING,  `New Unit ${UnitsType.Stick}$ (R) (T: 10x)`, TEXT_STYLE)
+        const newUnitText = this.scene.add.text(
+            width / 2 - OVERLAY_WIDTH / 2 + OVERLAY_PADDING,
+            height - OVERLAY_HEIGHT + OVERLAY_PADDING,
+            `New Unit ${UnitsType.Stick}$ (R) (T: 10x)`,
+            TEXT_STYLE
+        )
         newUnitText.setInteractive()
 
         this.selectedTown = town
 
         newUnitText.on(Phaser.Input.Events.POINTER_UP, () => {
-            if(this.selectedTown) {
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
+            if (this.selectedTown) {
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
             }
         })
 
         this.shapes.push(rectangle)
         this.shapes.push(newUnitText)
 
-        const newUnitKey = this.scene.input.keyboard.addKey(NEW_UNIT_SHORTCUT_BIS, true, true);
-        newUnitKey.on('down', () =>  {
-            if(this.selectedTown) {
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
+        const newUnitKey = this.scene.input.keyboard.addKey(
+            NEW_UNIT_SHORTCUT_BIS,
+            true,
+            true
+        )
+        newUnitKey.on('down', () => {
+            if (this.selectedTown) {
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
             }
-        });
-        const newUnit10XKey = this.scene.input.keyboard.addKey(NEW_UNIT_10X_SHORTCUT_BIS, true, true);
-        newUnit10XKey.on('down', () =>  {
-            if(this.selectedTown) { this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
-                this.scene.actions.newUnit(this.selectedTown.x, this.selectedTown.y)
+        })
+        const newUnit10XKey = this.scene.input.keyboard.addKey(
+            NEW_UNIT_10X_SHORTCUT_BIS,
+            true,
+            true
+        )
+
+        newUnit10XKey.on('down', () => {
+            if (this.selectedTown) {
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
+                this.scene.actions.newUnit(
+                    this.selectedTown.x,
+                    this.selectedTown.y
+                )
             }
-        });
+        })
     }
 
     onEmptyTileSelected() {
-        this.scene.input.keyboard.removeKey(NEW_UNIT_SHORTCUT_BIS);
-        this.scene.input.keyboard.removeKey(NEW_UNIT_10X_SHORTCUT_BIS);
-        this.shapes.forEach(shape => {
+        this.scene.input.keyboard.removeKey(NEW_UNIT_SHORTCUT_BIS)
+        this.scene.input.keyboard.removeKey(NEW_UNIT_10X_SHORTCUT_BIS)
+        this.shapes.forEach((shape) => {
             shape.removeAllListeners()
             shape.destroy()
         })
         this.shapes = []
         this.selectedTown = null
-
     }
 }
