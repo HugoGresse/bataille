8bd1f067246735a67408fbb39837a016d59bc912
Hugo Gresse <hugo.gresse@gmail.com>
2021-08-16 18:27:56 +0200

    Reduce sound volume and fix missing country polygon CZ

--- src/client/game/utils/sounds.ts
+++ src/client/game/utils/sounds.ts
@@ -1,7 +1,11 @@
 export const playStartSound = () => {
-    new Audio('/assets/audio/poupoupoupou.mp3').play()
+    const audio = new Audio('/assets/audio/poupoupoupou.mp3')
+    audio.volume = 0.7;
+        audio.play()
 }
 
 export const playTownCapturedSound = () => {
-    new Audio('/assets/audio/pipou.mp3').play()
+    const audio = new Audio('/assets/audio/pipou.mp3')
+    audio.volume = 0.5;
+    audio.play()
 }
