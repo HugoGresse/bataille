366d3c44f90dc2abdb69008bb8a7990e57424aa1
Hugo Gresse <hugo.gresse@gmail.com>
2021-08-16 21:29:04 +0200

    Min Two players to force start the game

--- src/client/Lobby.tsx
+++ src/client/Lobby.tsx
@@ -1,25 +1,25 @@
-import React, {useEffect, useState} from 'react'
-import {Box, Button,  Typography} from '@material-ui/core'
-import {DonatingBanner} from './screens/DonatingBanner'
-import {Link as RouterLink, useHistory} from 'react-router-dom'
-import {getSocketConnectionInstance, newSocketConnectionInstance} from './SocketConnection'
-import {ArrowBack} from '@material-ui/icons'
-import {LobbyState} from '../server/GameLobby'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Typography } from '@material-ui/core'
+import { DonatingBanner } from './screens/DonatingBanner'
+import { Link as RouterLink, useHistory } from 'react-router-dom'
+import { getSocketConnectionInstance, newSocketConnectionInstance } from './SocketConnection'
+import { ArrowBack } from '@material-ui/icons'
+import { LobbyState } from '../server/GameLobby'
 
 export const Lobby = () => {
-    const history = useHistory();
+    const history = useHistory()
     const [lobbyState, setLobbyState] = useState<LobbyState>({
         playerCountForceStart: 0,
         requiredPlayerCount: 0,
-        playerCount: 0
+        playerCount: 0,
     })
-    const [forceStart, setForceStart]  = useState(false)
+    const [forceStart, setForceStart] = useState(false)
 
     const onForceStartPress = () => {
         const forceStartValue = !forceStart
         setForceStart(forceStartValue)
         const socketConnection = getSocketConnectionInstance()
-        if(socketConnection) {
+        if (socketConnection) {
             socketConnection.sendForceStart(forceStartValue)
         }
     }
@@ -28,34 +28,40 @@ export const Lobby = () => {
         newSocketConnectionInstance(
             (lobbyState) => {
                 setLobbyState(lobbyState)
-        },(gameId: string) => {
+            },
+            (gameId: string) => {
                 history.push(`/g/${gameId}/`)
             }
         )
     }, [history])
 
+    return (
+        <Box
+            display="flex"
+            flex={1}
+            minHeight="100vh"
+            alignItems="center"
+            justifyContent="space-between"
+            flexDirection="column">
+            <Box>
+                <br />
+                <Button variant="contained" size="large" component={RouterLink} to="/" startIcon={<ArrowBack />}>
+                    Go back
+                </Button>
+            </Box>
 
-    return <Box display="flex" flex={1} minHeight="100vh" alignItems="center" justifyContent="space-between"
-                flexDirection="column">
-        <Box>
-            <br/>
-            <Button variant="contained" size="large"  component={RouterLink} to="/" startIcon={<ArrowBack/>}>
-                Go back
-            </Button>
+            <Box>
+                <Typography variant="h3">
+                    Waiting for more players... {lobbyState.playerCount}/{lobbyState.requiredPlayerCount}
+                </Typography>
+                <br />
+                <Button variant={forceStart ? 'contained' : 'outlined'} size="large" onClick={onForceStartPress}>
+                    Force start ({lobbyState.playerCountForceStart}/
+                    {lobbyState.playerCount > 1 ? lobbyState.playerCount : 2})
+                </Button>
+            </Box>
+
+            <DonatingBanner />
         </Box>
-
-        <Box>
-            <Typography variant="h3">Waiting for more players... {lobbyState.playerCount}/{lobbyState.requiredPlayerCount}</Typography>
-            <br/>
-            <Button variant={forceStart? "contained": "outlined"} size="large" onClick={onForceStartPress}>
-                Force start ({lobbyState.playerCountForceStart}/{lobbyState.playerCount})
-            </Button>
-
-        </Box>
-
-        <DonatingBanner/>
-
-    </Box>
-
-
+    )
 }
