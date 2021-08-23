cb4b96517155a907c0cedc84e79fc607bde00e49
Hugo Gresse <hugo.gresse@gmail.com>
2021-08-23 21:34:50 +0200

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'bataille-server',
+            script: 'lib/src/server/server.js',
+            watch: 'lib/',
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+}
