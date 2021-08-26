3e2a2cbdba83b0e8eaa13c586ffc47f32a3ae907
Hugo Gresse <hugo.gresse@gmail.com>
2021-08-26 11:51:59 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
             name: 'bataille-server',
             script: 'lib/src/server/server.js',
             watch: 'lib/',
+            log_date_format: "YYYY-MM-DD HH:mm:ss Z",
             env: {
                 NODE_ENV: 'production',
                 SUMOLOGIC_COLLECTOR: 'https://example.com',
