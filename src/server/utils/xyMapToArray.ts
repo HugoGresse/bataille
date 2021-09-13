f0543e2372c5f3daa623309f3f76ccabb173451f
Hugo Gresse <hugo.gresse@gmail.com>
2021-09-13 13:15:56 +0200

    Remove unused methods

--- src/server/utils/xyMapToArray.ts
+++ src/server/utils/xyMapToArray.ts
@@ -1,37 +1,5 @@
-import { getKeys } from './getKeys'
-
 export interface XYMap {
     [x: number]: {
         [y: number]: any
-    }
-}
-
-export interface XYMapWithType<T> {
-    [x: number]: {
-        [y: number]: T
-    }
-}
-
-export function xyMapToArray<Type>(map: XYMap): Type[] {
-    const results = []
-    const xKeys = getKeys(map)
-    let yKeys = []
-    for (const x of xKeys) {
-        yKeys = getKeys(map[x])
-        for (const y of yKeys) {
-            results.push(map[x][y])
-        }
-    }
-    return results
-}
-
-export function iterateOnXYMap<Type>(map: XYMap, func: (type: Type, x: number, y: number) => void): void {
-    const xKeys = getKeys(map)
-    let yKeys = []
-    for (const x of xKeys) {
-        yKeys = getKeys(map[x])
-        for (const y of yKeys) {
-            func(map[x][y], x, y)
-        }
     }
 }
