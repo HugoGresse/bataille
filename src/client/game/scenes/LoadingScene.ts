71ffd35665b94ccd5e2533e58b2cfb9ff2d6f92e
Hugo Gresse <hugo.gresse@gmail.com>
2021-09-28 18:51:55 +0200

    Add ts to assets file name???

--- src/client/game/scenes/LoadingScene.ts
+++ src/client/game/scenes/LoadingScene.ts
@@ -5,12 +5,12 @@ export class LoadingScene extends Phaser.Scene {
         super('LoadingScene')
     }
     preload() {
-        this.load.image('tiles', '/assets/tilemaps/tiles/tile.png')
+        this.load.image('tiles', '/assets/tilemaps/tiles/tile.png?ts=1')
         this.load.spritesheet('tilesSpriteSheet', '/assets/tilemaps/tiles/tile.png', {
             frameWidth: 32,
             frameHeight: 32,
         })
-        this.load.tilemapTiledJSON('map', '/assets/tilemaps/json/map.json')
+        this.load.tilemapTiledJSON('map', '/assets/tilemaps/json/map.json?ts=1')
     }
 
     create(): void {
