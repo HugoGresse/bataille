4a26c3aef0b31cfc37cdbcdada5018c3a0f3e5a9
Hugo Gresse <hugo.gresse@gmail.com>
2022-09-10 17:54:07 +0200

    Add player name selection∏ (#6)

--- src/client/screens/Home.tsx
+++ src/client/screens/Home.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { Box, Button, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Box, Button, TextField, Typography } from '@material-ui/core'
 import { DonatingBanner } from './DonatingBanner'
 import { Link as RouterLink } from 'react-router-dom'
 import { HelpDialogButton } from './HelpDialog'
+import { pickRandomPlayerName } from '../../utils/pickRandomPlayerName'
+import { getSavedPlayerName, setPlayerNamePersistent } from '../utils/cookie'
 
 export const Home = () => {
+    const [playerName, setPlayerName] = useState(getSavedPlayerName() || pickRandomPlayerName())
+
     return (
         <Box
             display="flex"
@@ -17,14 +21,35 @@ export const Home = () => {
 
             <Box>
                 <Typography variant="h1">Bataille</Typography>
-                <Button variant="contained" size="large" component={RouterLink} to="/lobby">
-                    PLAY
-                </Button>
-                <HelpDialogButton
-                    style={{
-                        marginLeft: 68,
-                    }}
-                />
+                <Box>
+                    <Button variant="contained" size="large" component={RouterLink} to="/lobby">
+                        PLAY
+                    </Button>
+                    <HelpDialogButton
+                        style={{
+                            marginLeft: 68,
+                        }}
+                    />
+                </Box>
+
+                <Box marginTop={3}>
+                    <TextField
+                        value={playerName}
+                        inputProps={{ minLength: 2, maxLength: 20 }}
+                        fullWidth
+                        size="small"
+                        label="Player name (2<->20 chars)"
+                        onChange={(e) => {
+                            let name = e.target.value
+                            if (name.length > 0 && name.trim().length === 0) {
+                                name = pickRandomPlayerName()
+                            }
+
+                            setPlayerName(name)
+                            setPlayerNamePersistent(name)
+                        }}
+                    />
+                </Box>
             </Box>
 
             <DonatingBanner />
