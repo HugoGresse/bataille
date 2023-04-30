ddd5a7f15640d94ac8d80dd0cc6a10bcf154dce0
Hugo Gresse <hugo.gresse@gmail.com>
2023-04-30 19:02:08 +0200

    Update sockerio server

--- src/server/utils/io.ts
+++ src/server/utils/io.ts
@@ -1,12 +1,9 @@
 import { Server } from 'socket.io'
-import { createServer } from 'http'
 import { isProduction, PORT } from './serverEnv'
-
-const httpServer = createServer()
 
 const allowedOrigins = isProduction ? ['https://bataille.ovh', 'https://admin.socket.io'] : '*'
 
-export const socketIOServer = new Server(httpServer, {
+export const socketIOServer = new Server({
     cors: {
         origin: allowedOrigins,
         credentials: true,
